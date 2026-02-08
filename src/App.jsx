a67d2a3885642c683256e48361c81470c42ad2b7
Aayushm124 <aayushmishra600@gmail.com>
2026-02-08 13:03:28 +0530

    addded a home.jsx favorite.jsx MovieCard.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+// import MovieCard from "./assets/components/MovieCard";
+import Home from "./assets/pages/Home";
+// function App(){
+//   return(
+//     <>
+//     <Text display = "whats up "   />
+
+//     <Text display=  "hello"     />
+//     </>
+//   )
+// }
+
+// function Text({display}){
+//   return(
+//     <div>
+//       <p>{display}</p>
+//     </div>
+//   )}
+
+// function App() {
+//   const movieNumber = 1;
+
+//   return (
+//     <>
+//       {movieNumber === 1 ? (
+//         <MovieCard
+//           movie={{
+//             title: "Tim's film",
+//             release_date: "2024",
+//           }}
+//         />
+//       ) : (
+//         <MovieCard
+//           movie={{
+//             title: "joe's film",
+//             release_date: "2024",
+//           }}
+//         />
+//       )}
+//     </>
+//   );
+// }
+
 
 function App(){
-  return(
-    <>
-    <div>Hello my </div></>
-  )
+      return(
+        <>
+        <Home />
+        </>
+      )
 }
-export default App
+
+
+export default App;
---
+++ src/assets/pages/Home.jsx
@@ -0,0 +1,24 @@
+import MovieCard from "../components/MovieCard"
+
+
+function Home(){
+
+    const movies = [
+        {id: 1, title: "john wick", release_date:"2020"},
+        {id: 2, title: "terminator", release_date:"1999"},
+        {id: 3, title: "The Matrix", release_date:"1998"}
+    ]
+    
+    return <div className="home">
+
+        <div className="movies-grid" >
+            {movies.map(movie => (
+                <MovieCard movie ={movie} key = {movie.id}/>
+            ))
+        }
+
+        </div>
+    </div>
+}
+
+export default Home
